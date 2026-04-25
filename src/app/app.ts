3283edc39c94e1d5565a07a3f2a5bd35925f653c
Tran Huy <huyhtranse@gmail.com>
2026-04-25 17:46:51 +0700

    feat: add header navigation component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavModule, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('binance-dashboard');
+  protected readonly title = signal('so-thu-chi-dashboard');
 }
---
+++ src/app/core/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatBadgeModule
+];
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})
+export class MaterialModule { }
